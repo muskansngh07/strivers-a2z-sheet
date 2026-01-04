389189c597729356c08e7baa4ce77abf8d05a72a
Muskan <muskansngh15@gmail.com>
2026-01-05 00:49:48 +0530

    Count number of digits in the number

---
+++ basic_maths/countDigits.java
@@ -0,0 +1,20 @@
+package basic_maths;
+import java.util.*;
+public class countDigits {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num=sc.nextInt();
+        int count=0;
+        int temp=num;
+        while(num!=0)
+        {
+            int x=num%10;
+            count++;
+            num/=10;
+        }
+        System.out.println("The number of digits in "+temp+" is: "+count);
+        sc.close();
+    }
+}
