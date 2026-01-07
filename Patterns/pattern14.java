8c5128f16780ecd32c4f978a27c500d4cc86ebd9
Muskan <muskansngh15@gmail.com>
2026-01-07 23:24:30 +0530

    Added pattern 14 to the folder

---
+++ Patterns/pattern14.java
@@ -0,0 +1,14 @@
+package Patterns;
+public class pattern14 {
+    public static void main(String args[])
+    {
+        for(int i=69;i>=65;i--)
+        {
+            for(int j=65;j<=i;j++)
+            {
+                System.out.print((char)j);
+            }
+            System.out.println();
+        }
+    }
+}
