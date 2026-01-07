7ae14a91945d7efe54b16cee4a8770e303159e0a
Muskan <muskansngh15@gmail.com>
2026-01-07 23:32:08 +0530

    Added pattern 16

---
+++ Patterns/pattern16.java
@@ -0,0 +1,14 @@
+package Patterns;
+public class pattern16 {
+    public static void main(String args[])
+    {
+        for(int i=69;i>=65;i--)
+        {
+            for(int j=i;j<=69;j++)
+            {
+                System.out.print((char)j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
